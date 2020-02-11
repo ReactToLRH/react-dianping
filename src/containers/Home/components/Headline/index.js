32d4288a98e87baf1fcd71700e9074084e3c7a60
lrh <837233792@qq.com>
2020-02-11 23:09:19 +0800

    点评头条

---
+++ src/containers/Home/components/Headline/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Slider from 'react-slick';
+import './style.css';
+import { dataSource } from './headlineData';
+
+const headlineDataSource = dataSource;
+
+class Headline extends Component {
+  render () {
+    const settings = {
+      slidesToShow: 1,
+      swipeToSlide: true,
+      autoplay: true,
+      vertical: true
+    };
+
+    return (
+      <div className="headline">
+        <div className="headline__logo"></div>
+        <div className="headline__slider">
+          <Slider {...settings}>
+            {
+              headlineDataSource.map((item, index) => {
+                return (
+                  <a
+                    key={index}
+                    className="headline__sliderInner"
+                    href={item.url}
+                  >
+                    <div className="headline__sliderTitle">{item.title}</div>
+                    <div className="headline__sliderImgWrapper">
+                      <img className="headline__sliderImg" src={item.pic} alt={item.title} />
+                    </div>
+                  </a>
+                )
+              })
+            }
+          </Slider>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Headline;
