0acff380a0a374c14fdc64ebe0ca93e87c7cd99c
lrh <837233792@qq.com>
2020-02-12 13:37:13 +0800

    产品详情页基础组件开发

---
+++ src/containers/ProductDetail/components/BuyButton/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css'
+
+class BuyButton extends Component {
+  render() {
+    return (
+      <a className="buyButton" href="/">立即购买</a>
+    );
+  }
+}
+
+export default BuyButton;
