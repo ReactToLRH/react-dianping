8f5c4e4b18b78e92dbfbba34281221b7f7af0461
lrh <837233792@qq.com>
2020-02-13 11:05:07 +0800

    订单购买

--- src/containers/ProductDetail/components/BuyButton/index.js
+++ src/containers/ProductDetail/components/BuyButton/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import './style.css'
 
 class BuyButton extends Component {
   render() {
+    const { productId } = this.props;
     return (
-      <a className="buyButton" href="/">立即购买</a>
+      <Link className="buyButton" to={`/purchase/${productId}`}>
+        立即购买
+      </Link>
     );
   }
 }
